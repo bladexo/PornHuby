f642d9b4f9c45175c1858de922ce6d316df6e5d7
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-09 00:45:40 +0100

    Update config.py

--- PornHub/config.py
+++ PornHub/config.py
@@ -4,7 +4,7 @@ API_ID: int = 25803426
 API_HASH: str = "291b6bea4848d7606c0d3213c317b430"
 TOKEN: str = "6483499681:AAHEGzswREgXraiX02MCrDq5VvGE_9AWOqU"
 
-log_chat: int = -1001662591986
+log_chat: int = -1001902012951
 sub_chat: str = "SakuraEmpireTeam"
 sudoers: List[int] = [2137482758, 1249591948]
 prefixs: List[str] = ["/", "!", ".", "$", "-"]
